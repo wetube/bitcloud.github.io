468a53b605f70c707fcecc26b70fa35f20a8ec5b
P4V4 <speedload123@gmail.com>
2014-03-10 02:55:03 +0100

    Updated sub-menu

---
+++ src/script.js
@@ -0,0 +1,16 @@
+// vars
+
+menuClicked = false;
+
+$( document ).ready(function() {
+	$("nav #case #subnav-icon").click(function() {
+		if(menuClicked == true) {
+			$("nav #case #subnav").show(0);
+			menuClicked = false;
+		}
+		else {
+			$("nav #case #subnav").hide(0);
+			menuClicked = false;
+		}
+	});
+});
