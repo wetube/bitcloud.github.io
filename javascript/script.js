64aaa001bdf5b16fd766404d5802cc50497d3ec6
P4V4 <speedload123@gmail.com>
2014-03-12 18:54:13 +0100

    Clean Up

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,2 @@
-// vars
-
-var menuClicked = false;
-
 $( document ).ready(function() {
-	/*
-	$("nav #case #subnav-icon").click(function() {
-		if(menuClicked == true) {
-			$("nav #case #subnav").show(0);
-			menuClicked = false;
-		}
-		else {
-			$("nav #case #subnav").hide(0);
-			menuClicked = false;
-		}
-	});
-	*/
 });
