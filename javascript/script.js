4f59050ff3ffd3955f5ed8613c984a7708d91351
Victor Kim <victorkim360@gmail.com>
2014-08-12 16:47:56 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,11 @@
 $( document ).ready(function() {
+  ( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
 });
