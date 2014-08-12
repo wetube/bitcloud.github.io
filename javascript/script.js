36a8ee71269cb3697c36a7269f26b57ae31112fc
Victor Kim <victorkim360@gmail.com>
2014-08-12 17:08:05 -0600

    Revert c6579cb..cfb57c9
    This rolls back to commit c6579cbb06f1420f36cdb525ef17591da434dff7.

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,2 @@
 $( document ).ready(function() {
-
-
-( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
-    });
-} )( jQuery );
-
-
 });
